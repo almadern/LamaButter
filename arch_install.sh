3fface4767020b197a872d329e02680a8393a43c
Almadern <almadernshepord@gmail.com>
2021-03-23 23:54:02 +0300

    change config st and add zathura for script

--- arch_install.sh
+++ arch_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S pulseaudio pulseaudio-alsa pulsemixer xorg-server xorg-apps xcompmgr ranger alsa-utils git scrot discord ntfs-3g
+sudo pacman -S pulseaudio pulseaudio-alsa pulsemixer xorg-server xorg-apps xcompmgr ranger alsa-utils git scrot discord ntfs-3g zathura zathura-pdf-poppler
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
--- st-8.2/st-0.8.2/config.h
+++ st-8.2/st-0.8.2/config.h
@@ -171,8 +171,8 @@ MouseKey mkeys[] = {
 };
 
 /* Internal keyboard shortcuts. */
-#define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define MODKEY Modr4Mask
+#define TERMMOD (Mod4Mask|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -180,8 +180,8 @@ static Shortcut shortcuts[] = {
         { ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
         { ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
         { XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-        { TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-        { TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+        { TERMMOD,              XK_equal,       zoom,           {.f = +1} },
+        { TERMMOD,              XK_minus,       zoom,           {.f = -1} },
         { TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
         { TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
         { TERMMOD,              XK_V,           clippaste,      {.i =  0} },
