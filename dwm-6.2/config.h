f7584061c56738214dbdf41f0fd02451f0a851bf
Almadern <almadernshepord@gmail.com>
2021-04-03 22:49:47 +0300

    delete gaps

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -4,10 +4,6 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int gappx              = 5;        /* gaps between windows */
-static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
