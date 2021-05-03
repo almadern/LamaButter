b4b11cad95b26d352c343af31e2b28ffcedf1e07
Almadern <almadernshepord@gmail.com>
2021-05-04 02:29:35 +0300

    fix problem with gaps and change color

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int gappx              = 5;        /* gaps between windows */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
@@ -19,10 +19,11 @@ static const char col_gray3[]       = "#ffffff";
 static const char col_gray4[]       = "#eeeeee";
 //top bar second color (blue) and active window border color 
 static const char col_cyan[]        = "#005577";
+static const char col_pick[]        = "#35fc03";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_pick  },
 };
 
 /* tagging */
