b2f702c535eb1bf6501d120363a59e484d0a5273
Almadern <almadernshepord@gmail.com>
2021-05-12 23:12:30 +0300

    copy dwm from master(without status patch)

--- dwm-6.2/dwm.c
+++ dwm-6.2/dwm.c
@@ -270,7 +270,7 @@ static Cur *cursor[CurLast];
 static Clr **scheme;
 static Display *dpy;
 static Drw *drw;
-static Monitor *mons, *selmon, *statmon;
+static Monitor *mons, *selmon;
 static Window root, wmcheckwin;
 
 /* configuration, allows nested code to access above variables */
@@ -741,7 +741,7 @@ drawbar(Monitor *m)
 	Client *c;
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == statmon) { /* status is only drawn on user-defined status monitor */
+	if (m == selmon) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		sw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
 		drw_text(drw, m->ww - sw, 0, sw, bh, 0, stext, 0);
@@ -1943,7 +1943,7 @@ updategeom(void)
 				else
 					mons = createmon();
 			}
-			for (i = 0, m = mons; i < nn && m; m = m->next, i++){
+			for (i = 0, m = mons; i < nn && m; m = m->next, i++)
 				if (i >= n
 				|| unique[i].x_org != m->mx || unique[i].y_org != m->my
 				|| unique[i].width != m->mw || unique[i].height != m->mh)
@@ -1956,10 +1956,6 @@ updategeom(void)
 					m->mh = m->wh = unique[i].height;
 					updatebarpos(m);
 				}
-				if(i == statmonval)
-					statmon = m;
-			}
-
 		} else { /* less monitors available nn < n */
 			for (i = nn; i < n; i++) {
 				for (m = mons; m && m->next; m = m->next);
@@ -1973,8 +1969,6 @@ updategeom(void)
 				}
 				if (m == selmon)
 					selmon = mons;
-				if (m == statmon)
-					statmon = mons;
 				cleanupmon(m);
 			}
 		}
@@ -2062,7 +2056,7 @@ updatestatus(void)
 {
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
 		strcpy(stext, "dwm-"VERSION);
-	drawbar(statmon);
+	drawbar(selmon);
 }
 
 void
