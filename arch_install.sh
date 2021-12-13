748926560e7cc91460650349e6675597903df493
almadern <almadernshepord@gmail.com>
2021-12-13 01:26:55 +0000

    add service in first setup installation

--- arch_install.sh
+++ arch_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo pacman -S pulseaudio pulseaudio-alsa pulsemixer xorg-server xorg-apps xcompmgr ranger alsa-utils scrot discord ntfs-3g zathura zathura-pdf-poppler vlc firefox xorg-xinit virtualbox virtualbox-host-dkms linux-headers kitty
+sudo pacman -S pulseaudio pulseaudio-alsa pulsemixer xorg-server xorg-apps xcompmgr ranger alsa-utils scrot discord ntfs-3g zathura zathura-pdf-poppler firefox xorg-xinit virtualbox virtualbox-host-dkms linux-headers kitty papirus-icon-theme evolution evolution-ews gnome-keyring zsh zsh-syntax-highlighting lxappearance breeze-gtk mpv youtube-dl keepassxc
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 yay -S spotify
 yay -S xkb-switch-git
 yay -S evernote-beta-bin
+yay -S notion-app
+yay -S nerd-fonts-complete
